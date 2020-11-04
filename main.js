1ff6a0896427254584fcb79a7de565cd915851d3
NickSpangler <Spangleyman@mac.com>
2020-11-04 10:53:00 -0500

    Done.

--- main.js
+++ main.js
@@ -4,8 +4,48 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.querySelector('#modal');
+const modalMessage = document.querySelector('#modal-message');
+const modalSwitch = false;
+modal.classList.add('hidden');
 
+const hearts = document.querySelectorAll('span.like-glyph');
 
+function changeModal(msg) {
+  modal.classList.remove("hidden");
+  modalMessage.innerHTML += msg;
+  setTimeout(function() {
+    modal.classList.add('hidden');
+    modalMessage.innerHTML = "";
+  }, 5000);
+}
+
+function activateHeart(heart) {
+  heart.innerHTML = FULL_HEART;
+  heart.classList.add('activated-heart')
+}
+
+function deactivateHeart(heart) {
+  heart.innerHTML = EMPTY_HEART;
+  heart.classList.remove('activated-heart')
+}
+
+for (heart of hearts) {
+  heart.addEventListener('click', function(e) {
+    const heart = e.target
+    mimicServerCall()
+    .then(response => {
+      if (heart.classList.contains('activated-heart')) {
+        deactivateHeart(heart);
+      } else {
+        activateHeart(heart);
+      }
+    })
+    .catch(error => {
+      changeModal(error);
+    })
+  })
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
